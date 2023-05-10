6f896003c529f47609730f80fa1d1f5242666336
Адитья <78430010+itzrexmodz@users.noreply.github.com>
2023-05-10 15:17:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,5 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
    return 'Hello, World!'
-os.system("curl -sL https://deb.nodesource.com/setup_18.x | -E bash -")
+
+os.system("curl -sL https://deb.nodesource.com/setup_16.x -o /tmp/nodesource_setup.sh")
+os.system("bash /tmp/nodesource_setup.sh")
 os.system("pip3 install -U -r requirements.txt && nohup python3 -m YukkiMusic &")
